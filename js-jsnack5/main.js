5f7800794e232eaa141b87ba89d9823e06432bb8
Alba De biase <debiasealba@gmail.com>
2020-10-17 15:02:47 +0200

    esercizio 5

---
+++ js-jsnack5/main.js
@@ -0,0 +1,21 @@
+// stampa il cubo dei primi N numeri. Dove N è un numero indicato dall'utente.
+
+
+
+// Chiedere un numero all'utente
+
+var numeroN = parseInt(prompt("numero da inserire"));
+console.log(numeroN);
+
+for (var i = 1; i <= numeroN; i++) {
+    console.log(i);
+var cubo = Math.pow(i,3);
+console.log(cubo);
+
+}
+
+
+//
+// var numeroN math.pow( numeroN, 3);
+// var numeroN * cubo = cuboNumeri;
+// }
