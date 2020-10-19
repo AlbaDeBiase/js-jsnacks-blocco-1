b393508f06110da6f9f6755c9daad7529e7dfbb8
Alba De biase <debiasealba@gmail.com>
2020-10-19 19:05:11 +0200

    esercizio 6

---
+++ js-jsnack6/main.js
@@ -0,0 +1,10 @@
+// Stampa le potenze di 2 senza superare il numero 1000.
+
+// stampare le potenze di 2
+
+
+for (var i = 1; Math.pow(2, i) < 1000; i++) {
+    console.log(Math.pow(2, i));
+}
+//
+// non devo superare 1000;
