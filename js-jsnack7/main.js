9fa802a095c9d9549ab1f6bb37a004e333c2a99c
Alba De biase <debiasealba@gmail.com>
2020-10-19 20:39:31 +0200

    esercizio 7

---
+++ js-jsnack7/main.js
@@ -0,0 +1,11 @@
+// calcola la somma e la media dei numeri da 1 a 10
+
+var somma=0;
+ for (var i = 1; i <= 10; i++) {
+     console.log(i);
+somma=somma+i;
+ }
+console.log(somma);
+
+var media= somma/(i - 1);
+console.log(media);
